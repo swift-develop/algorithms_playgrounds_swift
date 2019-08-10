8e50bf4b2805868a2ecfef6de3a1d3cfe45e5279
Tom Grant <swift.developer@icloud.com>
2019-08-09 20:57:13 -0700

    wip

--- FrogJmp FrogJmp Frogjmp/FrogJmp FrogJmp Frogjmp/main.swift
+++ FrogJmp FrogJmp Frogjmp/FrogJmp FrogJmp Frogjmp/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
+public func solution(_ X : Int, _ Y : Int, _ D : Int) -> Int {
+    
 
+    
+    let distance = Y - X
+    let jumps = distance % D == 0 ? distance / D : distance / D + 1
+    
+    return jumps
+}
+
+
+let X = 10, Y = 21 , D = 5
+
+
+let r = solution(X, Y, D)
+
+print( r )
