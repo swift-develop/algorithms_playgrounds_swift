b596e6501207cb31bcb825c2e89ee38fd5d8821d
Tom Grant <swift.developer@icloud.com>
2019-08-11 16:05:55 -0700

    wip

--- chessBoardCellColor/chessBoardCellColor/main.swift
+++ chessBoardCellColor/chessBoardCellColor/main.swift
@@ -2,36 +2,24 @@
 //  main.swift
 //  chessBoardCellColor
 //
-//  Created by Tom Grant on 8/10/19.
+//  Created by Tom Grant on 8/11/19.
 //  Copyright © 2019 Tom Grant. All rights reserved.
 //
 
 import Foundation
 
+
+
 func chessBoardCellColor(cell1: String, cell2: String) -> Bool {
-    
-    let charOffset = "A".utf8.first! - 1
-    
-    guard let col1  = cell1.utf8.first! - charOffset
-        
-//          let row1 = cell1.utf8.last! - charOffset,
-//          let col1 = cell2.utf8.first! - charOffset,
-//          let row1 = cell2.utf8.last! - charOffset
-    
-    
-    else {
-        return false
-    }
-    
-//    print( col1, charOffset, col1 - charOffset )
-    print( type(of: col1 ))
-    
-    return false
-    
+
+    guard let a = cell1.utf8.first,let b = cell1.utf8.last,let c = cell2.utf8.first,let d = cell2.utf8.last else { return false }
+    return ( a + b + c + d ) % 2 == 0 ? true  : false
 }
 
 
-let r = chessBoardCellColor(cell1: "A1", cell2: "A2" )
+let r = chessBoardCellColor(cell1: "A1", cell2: "C3")
+print( r)
 
 
-print( r )
+
+print( (1 + 3) % 2 )
