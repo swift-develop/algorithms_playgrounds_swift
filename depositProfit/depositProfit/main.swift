c88f467106ccc34dffc473cbe88d9f87a9d4d90f
Tom Grant <swift.developer@icloud.com>
2019-08-11 20:46:29 -0700

    wip

--- depositProfit/depositProfit/main.swift
+++ depositProfit/depositProfit/main.swift
@@ -7,6 +7,27 @@
 //
 
 import Foundation
+class Solution {
+    func depositProfit(deposit: Int, rate: Int, threshold: Int) -> Int {
+        
+        let r =  Double(rate) / 100 + 1
+        var sum = Double( deposit )
+        let limit = Double( threshold )
+        var i = 0
+        
+        //print( sum, r, limit )
+        while sum < limit {
+            sum = sum * r
+            i += 1
+        }
+        return i
+    }
+    
+    func depositProfit_log(deposit: Int, rate: Int, threshold: Int) -> Int {
+        //return math.ceil(math.log(threshold/deposit, 1+rate/100))
+        return Int( ceil( log(Double(threshold)/Double(deposit)) / log(Double(rate) / 100 + 1) ) )
+    }
+    
+}
 
-print("Hello, World!")
-
+Solution().depositProfit_log(deposit: 100, rate: 20, threshold: 170)
--- depositProfit/test/test.swift
+++ depositProfit/test/test.swift
@@ -10,6 +10,13 @@ import XCTest
 
 class test: XCTestCase {
 
+    let s = Solution()
+    
+    let rate = 20
+    let deposit = 100
+    let threshold = 1000000000000000000
+    
+    var r = 0
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,16 +25,33 @@ class test: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test0() {
+        // This is an example of a performance test case.
+            // Put the code you want to measure the time of here.
+        r = s.depositProfit(deposit: 100, rate: 20, threshold: 170)
+        
+        print( r )
     }
+    
 
-    func testPerformanceExample() {
+    func test1() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+            r = s.depositProfit(deposit: deposit, rate: rate, threshold: threshold)
         }
+        
+        print( r )
+    }
+    
+    func test2() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+            r = s.depositProfit_log(deposit: deposit, rate: rate, threshold: threshold)
+        }
+        
+        print( r )
     }
 
 }
