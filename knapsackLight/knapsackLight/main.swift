ada881c1f6adc2e2183bd974cb8560990ee5b717
Tom Grant <swift.developer@icloud.com>
2019-08-13 10:27:47 -0700

    stuff

--- knapsackLight/knapsackLight/main.swift
+++ knapsackLight/knapsackLight/main.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 
-print("Hello, World!")
+func knapsackLight(value1: Int, weight1: Int, value2: Int, weight2: Int, maxW: Int) -> Int {
+    
+    guard weight1 <= maxW || weight2 <= maxW else { return 0 }
+    
+    if weight1 + weight2 <= maxW { return value1 + value2 }
+    
+    if weight1 <= maxW && weight2 <= maxW { return max(value1, value2 )}
+    
+    if weight1 > weight2 { return value2 }
 
+
+    
+    
+    
+    return value1
+    
+}
+
+
+let value1 = 2
+let weight1 = 5
+let value2 = 3
+let weight2 = 4
+let maxW = 5
+
+
+
+let result = knapsackLight(value1: value1, weight1: weight1, value2: value2, weight2: weight2, maxW: maxW)
+
+print( result )
