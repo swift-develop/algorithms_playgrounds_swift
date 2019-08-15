fe5368984fc351399d934f7fa542cbf36fdaa67b
Tom Grant <swift.developer@icloud.com>
2019-08-15 10:25:55 -0700

    wip

--- spiralNumbers/spiralNumbers/main.swift
+++ spiralNumbers/spiralNumbers/main.swift
@@ -8,5 +8,43 @@
 
 import Foundation
 
-print("Hello, World!")
+func spiralNumbers(n: Int) -> [[Int]] {
+    
+    var matrix = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
+    
+    let limit = n * n
+    var directions = [ (x:+1,y:0),(x:0,y:+1),(x:-1,y:0),(x:0,y:-1) ]
+    var cur = 0
+    var d = directions[cur]
+    
 
+    var x = 0, y = 0
+    var i = 1
+
+    
+    while i <= limit {
+        matrix[y][x] = i
+        let newX = x + d.x
+        let newY = y + d.y
+        if newX >= n || newY >= n || newX < 0 || newY < 0 || matrix[newY][newX] > 0 {
+            if cur == directions.count - 1 {
+                cur = 0
+            } else {
+                cur += 1
+            }
+            d = directions[cur]
+        }
+        x += d.x
+        y += d.y
+        i += 1
+    }
+    
+    return matrix
+}
+
+
+let n = 3
+
+let r = spiralNumbers(n: n)
+
+print( r )
