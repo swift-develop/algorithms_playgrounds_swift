b3a54ff1547828cf64f4bb2b6db6231f0edad5a6
Tom Grant <swift.developer@icloud.com>
2019-08-15 21:37:35 -0700

    sdfa

--- isCryptSolution/isCryptSolution/main.swift
+++ isCryptSolution/isCryptSolution/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+func isCryptSolution(crypt: [String], solution: [[Character]]) -> Bool {
+    var r = [Int]()
+    let hash = solution.reduce(into: [Character:Character]() ) { return $0[ $1[0] as Character ] = $1[1] }
 
+    
+    for w in crypt {
+        let s = String( w.map { hash[$0] ?? "0" } )
+        if w.count < 2 { r.append( Int(s) ?? -1)}
+        else if s.first == "0" { return false }
+        else { r.append( Int(s) ?? -2 )}
+    }
+    
+    print( r )
+    
+    return r[0] + r[1] == r[2]
+}
+
+
+let crypt = ["A",
+             "A",
+             "A"]
+
+let solution: [[Character]]  = [["A","0"]]
+
+
+let r = isCryptSolution( crypt: crypt, solution: solution )
+
+
+print( r )
