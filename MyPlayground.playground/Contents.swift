ee40e6abfd2fca59bd0827297ee0111b918b2563
Tom Grant <swift.developer@icloud.com>
2019-09-17 18:24:33 -0700

    wip

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,17 +1,26 @@
 import UIKit
 
-
-var x : Set<Int> = []
-
-10
-10 + 50
-10 + 50 + 100
+var str = "Hello, playground"
 
 
-50
-50 + 10
-50 + 10 + 50
+let x = String("int main(int argc, const char * argv[])".utf8CString )
 
-100
-100 + 10
-100 + 10 + 50
+let y = String("i​n​t​ ​m​a​i​n​(​i​n​t​ ​a​r​g​c​,​ ​c​o​n​s​t​ ​c​h​a​r​ ​*​ ​a​r​g​v​[​]​)".utf8CString )
+
+int main(int argc, const char * argv[]) {
+
+print( type(of: x), type(of: y))
+print( x.count, y.count )
+
+let z = zip(x, y)
+
+
+for (a,b) in z {
+    
+    print( a, b, type(of: a), type(of: b) )
+    
+}
+
+
+print( x == y )
+
---
+++ string_definition/venv/stringDefinition.py
@@ -0,0 +1,8 @@
+s = 'abacaba'
+s1 = "somestring"
+# s2 = ''somestring''
+# s3 = ""somestring""
+s4 = '''somestring2'''
+s5 = """somestring3"""
+s6 = """"somestring4"""""
+print( s,s1,s4,s5,s6 )
