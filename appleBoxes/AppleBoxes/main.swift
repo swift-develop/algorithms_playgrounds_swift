90cb8efc0bcebef6bb60db3d3f2f79a3af415830
Tom Grant <swift.developer@icloud.com>
2019-09-19 12:40:19 -0700

    solved

--- appleBoxes/AppleBoxes/main.swift
+++ appleBoxes/AppleBoxes/main.swift
@@ -8,5 +8,30 @@
 
 import Foundation
 
-print("Hello, World!")
+func appleBoxes(k: Int) -> Int {
+    
+//    let yellowApples = 0
+//    let redApples = 0
+    
 
+    
+//    for boxSize in 1...k {
+//
+//        let NumberApplesInBox = ( boxSize * boxSize )
+//
+//        apples += boxSize % 2 == 0 ? NumberApplesInBox : -NumberApplesInBox
+//    }
+    
+    
+    //return (1...k).reduce(0) { $0 + ( $1 * $1 ) * (( $1 % 2 == 0 ) ? 1 : -1 ) }
+    
+    return k * (k + 1) / ((k % 2 == 0) ? 2 : -2)
+}
+
+
+let k = 5
+
+
+let r = appleBoxes(k: k)
+
+print( r )
