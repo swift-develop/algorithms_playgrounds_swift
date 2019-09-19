24b04f4abdc18d4f49bdee1c704d66367a05ade6
Tom Grant <swift.developer@icloud.com>
2019-09-19 12:40:13 -0700

    solved

---
+++ appleBoxes/AppleBoxes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AppleBoxes
+//
+//  Created by Tom Grant on 9/19/19.
+//  Copyright © 2019 Tom Grant. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
