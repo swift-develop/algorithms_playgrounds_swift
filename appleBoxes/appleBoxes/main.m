d9d8c699f7db722072d08d2cff3220873bcb07a5
Tom Grant <swift.developer@icloud.com>
2019-09-19 12:13:06 -0700

    apple boxes

--- appleBoxes/appleBoxes/main.m
+++ appleBoxes/appleBoxes/main.m
@@ -2,40 +2,16 @@
 //  main.m
 //  appleBoxes
 //
-//  Created by Tom Grant on 9/13/19.
+//  Created by Tom Grant on 9/19/19.
 //  Copyright © 2019 Tom Grant. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-#import <stdio.h>
-
-
-
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
-        
-
-        NSString* myString = [NSString string];
-        
-        
-        float weight = 14.2;
-        
-        printf("The turkey weighs %f %f.\n", weight, 99.9 );
-        
-        
-        {
-            int x = 9;
-            printf( "%i is the value\n", x );
-        }
-        
-        
-        
     }
     return 0;
 }
-
-
-
