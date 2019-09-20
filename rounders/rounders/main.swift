0af1d27cdd4a614b64e7ae5debab0fbb94df3f72
Tom Grant <swift.developer@icloud.com>
2019-09-20 08:10:16 -0700

    solving

--- rounders/rounders/main.swift
+++ rounders/rounders/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+func rounders(n: Int) -> Int {
+    var n = n
+    var digits = 10
+    
+    repeat {
+       
+        //remove and round
+        var (q,r) = n.quotientAndRemainder(dividingBy: digits)
+        
+        if  r*10 / digits * 10  >= 5  {
+            q += 1
+        }
+        
+        n = q * digits
+        
+        print( n, q, r, digits )
+        
+        digits *= 10
+        
+    } while digits < n
+    
+    
+    return n
+}
 
+
+let n = 15
+
+let result = rounders(n:n)
+
+print( result )
